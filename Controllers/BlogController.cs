626cd5794a21070182c5bc63b908b526f13eaa05
句和一 <alice@aisys.dev>
2023-08-02 04:17:54 -0600

    Added blog

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Saaya.Web.Common.Attributes;
+using Saaya.Web.Db;
+using Saaya.Web.Db.Models;
+using Saaya.Web.Models;
+using Saaya.Web.Utility;
+using System.Data;
+using System.Diagnostics;
+
+namespace Saaya.Web.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ILogger<BlogController> _logger;
+        private readonly SaayaWebContext _db;
+
+        public BlogController(ILogger<BlogController> logger, SaayaWebContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult index()
+        {
+            return View(_db.Posts.OrderByDescending(x => x.DateCreated).ToList());
+        }
+
+        public IActionResult post(int id)
+        {
+            if (!_db.Posts.Where(x => x.Id == id).Any())
+            { return RedirectToAction(nameof(index)); }
+
+            return View(_db.Posts.Where(x => x.Id == id).FirstOrDefault());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorized(Role = nameof(StaticDetails.Admin))]
+        public async Task<IActionResult> add(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                if (post.Id == 0) { _db.Posts.Add(post); }
+                else { _db.Posts.Update(post); }
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(index));
+            }
+            else
+            {
+                if (post.Id != 0)
+                {
+                    post = _db.Posts.FirstOrDefault(x => x.Id == post.Id);
+                }
+            }
+
+            return View(post);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
