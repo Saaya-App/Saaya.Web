edf7309e6f69e4f1144fc35c809c895a78e88ea5
句和一 <alice@aisys.dev>
2023-08-09 10:02:43 -0600

    added WIP api docs page

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Saaya.Web.Controllers
+{
+    public class ApiController : Controller
+    {
+        public ApiController()
+        {
+            
+        }
+
+        public IActionResult docs()
+            => View();
+    }
+}
